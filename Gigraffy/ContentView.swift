a9ce7e5eb370022cc0c8568901e6e06bd5d17e4d
Neeraj Sharma <neeraj_sharma@starkey.com>
2020-11-06 14:47:37 -0600

    Initial Commit

---
+++ Gigraffy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gigraffy
+//
+//  Created by Neeraj Sharma on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gigraffy/GigraffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigraffyApp.swift
+//  Gigraffy
+//
+//  Created by Neeraj Sharma on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct GigraffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
